5da3dfcb7a9bc965f492ad118cdc09b22a97f8e3
shekhar9 <shekharvadla@gmail.com>
2024-10-01 20:34:29 +0530

    profile file merged

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jobportal',     
-        'USER': 'postgres',    
-        'PASSWORD': 'Vamshi',  
-        'HOST': 'localhost',            
-        'PORT': '5432',                   
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'jobportal',     
+#         'USER': 'postgres',    
+#         'PASSWORD': 'Vamshi',  
+#         'HOST': 'localhost',            
+#         'PORT': '5432',                   
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
